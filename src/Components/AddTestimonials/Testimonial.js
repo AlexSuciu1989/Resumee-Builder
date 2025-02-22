5318fa2165a7fd6bdf4a52b7fec76c0d481576da
Alex Suciu <v.alex.suciu@gmail.com>
2025-02-22 11:17:03 +0200

    adding testimonials

---
+++ src/Components/AddTestimonials/Testimonial.js
@@ -0,0 +1,12 @@
+
+function Testimonial () {
+    return (
+    <div>
+        <textarea></textarea>
+        <input type="text" placeholder="from who"></input>
+        <button>Delete</button>
+    </div>
+    )
+};
+
+export default Testimonial;
